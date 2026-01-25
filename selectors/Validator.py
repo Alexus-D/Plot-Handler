d21128d2dfcee0c8275988e813f220bf6da4a735
Alexey Kaminskiy <verycoolman77@gmail.com>
2026-01-25 19:36:37 +0300

    Add Validator class for handling validation logic with accept/deny modes

---
+++ selectors/Validator.py
@@ -0,0 +1,26 @@
+from .Selector import Selector
+
+
+class Validator(Selector):
+	def __init__(self,
+				 stage_name: str,
+				 plotter,
+				 markers = {},
+				 clear_button: bool = False,
+				 save_button: bool = False):
+		buttons = [("accept", "Accept"), ("deny", "Deny")]
+		super().__init__(stage_name, plotter, markers, buttons, clear_button, save_button)
+
+	def _set_mode(self, mode):
+		if mode == "accept":
+			self.params = {"Validation": True}
+			self.mode = None
+			return
+		if mode == "deny":
+			self.params = {"Validation": False}
+			self.mode = None
+			return
+		raise ValueError(f"Mode {mode} is not recognized")
+
+	def mouse_on_click(self, event):
+		return
